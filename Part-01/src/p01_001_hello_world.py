d08b132f0b4c90802cf8fa90589c3fc5a74d2e61
feelins <feipengshao@163.com>
2022-02-16 12:37:04 +0800

    更新第一个程序说明

--- Part-01/src/p01_001_hello_world.py
+++ Part-01/src/p01_001_hello_world.py
@@ -7,7 +7,7 @@
 @Version :   1.0
 @Contact :   feipengshao@163.com
 @License :   (C)Copyright 2022-2023, 极地语音工作室
-@Desc    :   打印hello world!
+@Desc    :   打印hello world
 '''
 
 # here put the import lib
@@ -16,5 +16,5 @@ print('hello world!')
 print("hello world!")
 print('hello, "Li"!')
 print("hello, 'Li'!")
-print('hello, \'Li\'!')
-print("hello, \"Li\"!")
+print('hello, \'Li\'!') # 外层用了单引号，内部还是想输出单引号时，加转义符号\
+print("hello, \"Li\"!") # 同上
