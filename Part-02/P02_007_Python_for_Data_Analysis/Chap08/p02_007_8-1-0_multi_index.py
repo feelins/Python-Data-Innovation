ef1ddaf44f075b1620fb2448896c4732dcc37b9f
feelins <feipengshao@163.com>
2023-12-25 19:05:39 +0800

    使用MultiIndex作为索引的美化视图

---
+++ Part-02/P02_007_Python_for_Data_Analysis/Chap08/p02_007_8-1-0_multi_index.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   p02_007_8-1-0_multi_index.py
+@Time    :   2023/12/25 17:39:03
+@Author  :   Feelins Shao 
+@Version :   1.0
+@Contact :   feipengshao@163.com
+@License :   (C)Copyright 2022-2023, Feelins Shao
+@Desc    :   8.1 分层索引
+'''
+
+
+import logging, time
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+
+    # 记录开始运行时间
+    start_time = time.time()
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info('开始: ')
+    
+    data = pd.Series(np.random.randn(9), index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], [1, 2, 3, 1, 3, 1, 2, 2, 3]])
+    logging.info('在一个轴向上拥有多个索引层级')
+    print(data)
+    logging.info('使用MultiIndex作为索引的美化视图')
+    print(data.index)
+    print(data['b'])
+    print(data['b' : 'c'])
+    print(data.loc[['b', 'd']])
+    print(data.loc[:, 2]) # 这个代表是a, b, c，第一层的所有行，这里面所有第二层索引为2的值
+    print(data.unstack())
+
+
+    # 记录结束时间，计算运行时间
+    end_time = time.time()
+    total_time = end_time - start_time
+    logging.info('结束：')
+    logging.info(f'总共用时: {total_time:.2f}秒')
+    logging.info('Done!')
