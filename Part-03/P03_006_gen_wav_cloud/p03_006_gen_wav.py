2ad2c16271c2e512da14eda67d31d601602aaf50
feelins <feipengshao@163.com>
2024-10-30 10:23:43 +0800

    更新cloud合成

--- Part-03/P03_006_gen_wav_cloud/p03_006_gen_wav.py
+++ Part-03/P03_006_gen_wav_cloud/p03_006_gen_wav.py
@@ -1,41 +1,21 @@
 import time
 import threading
 import sys
-import pyworld as pw
-import soundfile as sf
-import numpy as np
-
 import nls
 
-def read_numbers_from_file(file_path):
-    numbers = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            # 假设文件中的每一行都是一个数字或包含多个用空格分隔的数字
-            line_numbers = map(float, line.split())
-            numbers.extend(line_numbers)
-    return np.array(numbers, dtype=np.float64)
 
-# Convert speech into features (using default arguments)
-WAV_FILE = r'tests\test_tts_阿里-知小夏.wav'
-x, fs = sf.read(WAV_FILE)
-f0, sp, ap = pw.wav2world(x, fs)
-f0_FILE = r'tests\test_tts_阿里-知小夏.f0'
-with open(f0_FILE, 'w') as wid:
-    wid.writelines([str(x) + '\n' for x in f0])
-f0_FILE = r'tests\test_tts_阿里-知小夏.new.f0'
-f0_ = read_numbers_from_file(f0_FILE)
-y = pw.synthesize(np.array(f0_), sp, ap, fs)
-OUT_WAV_FILE = r'tests\test_tts_阿里-知小夏_out.wav'
-sf.write(OUT_WAV_FILE, y, fs)
 
+SPEAKER = ['zhifeng_emo', 'siyue']
+select_index = 1
+SPEAKER_RATE = 100
 URL="wss://nls-gateway-cn-shanghai.aliyuncs.com/ws/v1"
-TOKEN="d2ecff27a2814f6da888763ed17ca915"  #参考https://help.aliyun.com/document_detail/450255.html获取token
+TOKEN="e05a6425f4994af5958f18d8955f8f3c"  #参考https://help.aliyun.com/document_detail/450255.html获取token
 APPKEY="CKHXq4tUs26fQq6t"       #获取Appkey请前往控制台：https://nls-portal.console.aliyun.com/applist
 
-
-
-TEXT='你看一下这些图标中还有其它的交通工具吗?'
+inpt_text_file = r''
+with open(inpt_text_file, encoding='utf-8') as fid:
+    TEXT = fid.read()
+# TEXT='你看一下这些图标中还有其它的交通工具吗?'
 
 #以下代码会根据上述TEXT文本反复进行语音合成
 class TestTts:
@@ -84,13 +64,13 @@ class TestTts:
       	      	      	      	       on_close=self.test_on_close,
       	      	      	      	       callback_args=[self.__id])
       	print("{}: session start".format(self.__id))
-      	r = tts.start(self.__text, voice="xiaogang")
+      	r = tts.start(self.__text, voice=SPEAKER[select_index], speech_rate=SPEAKER_RATE)
       	print("{}: tts done with result:{}".format(self.__id, r))
 
 def multiruntest(num=500):
     for i in range(0, num):
         name = "thread" + str(i)
-        t = TestTts(name, "tests/test_tts_zhishuo99.pcm")
+        t = TestTts(name, "tests/test_tts_" + SPEAKER[select_index] + "_" + str(SPEAKER_RATE) + ".pcm")
         t.start(TEXT)
 
 nls.enableTrace(True)
